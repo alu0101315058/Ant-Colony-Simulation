0c2830a1847b8c7941ca407ee9ea939c49e5113f
Enrique Viña Alonso <alu0101337760@ull.edu.es>
2023-01-21 20:08:39 +0000

    Corrected typo

--- Assets/Scripts/Flock/Behaviour Scripts/AvoidCenter.cs
+++ Assets/Scripts/Flock/Behaviour Scripts/AvoidCenter.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Flock/Behaviour/Avoid Centre")]
-public class AvoidCentre : FlockBehaviour
+public class AvoidCenter : FlockBehaviour
 {
     public Vector2 centre;
     public float radius = 15f;
 
     public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
     {
-        Vector2 centreOffset = centre - (Vector2)agent.transform.position;
-        float t = centreOffset.magnitude;
+        Vector2 centerOffset = centre - (Vector2)agent.transform.position;
+        float t = centerOffset.magnitude;
         if (t > radius)
         {
             return Vector2.zero;
         }
         if (t == 0) t = 1;
-        return -centreOffset / (t*t);
+        return -centerOffset / (t*t);
     }
 }
